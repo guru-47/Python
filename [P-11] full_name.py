89b42c8f895026c801be8285d65063287dc65152
GURU PRASATH A <guruprasath1115@gmail.com>
2025-08-03 08:19:13 +0530

    Create [P-11] full_name.py

---
+++ [P-11] full_name.py
@@ -0,0 +1,9 @@
+first_name = input()
+last_name = input()
+print(first_name +" " + last_name)
+
+#first_name = Guru
+#last_name = Prasath A
+#print(first_name +" " + last_name)
+
+# op = Guru Prasath A
