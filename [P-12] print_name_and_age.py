eda7bfa3fd0fb2de5994f3f7daed6ad8eee2b55b
GURU PRASATH A <guruprasath1115@gmail.com>
2025-08-03 08:23:16 +0530

    Create [P-12] print_name_and_age.py

---
+++ [P-12] print_name_and_age.py
@@ -0,0 +1,9 @@
+name = input()
+age = input()
+print(name +  " is " + age + " years" + " old") # string concatination using '+'
+
+#name = Guru
+#age = 18
+#print(name +  " is " + age + " years" + " old")
+
+# op = Guru is 18 years old
