2e0894cfd3b17295d7d2842c2a812d06f9bb2945
GURU PRASATH A <guruprasath1115@gmail.com>
2025-08-09 21:33:02 +0530

    Create [P-28] star-repetation.py

---
+++ [P-28] star-repetation.py
@@ -0,0 +1,11 @@
+word = input()   # guru prasath
+
+a = word[0]      # g
+b = len(word) - 1 # 11 
+c = b * "*"      # **********
+
+result = a + c  # g + **********
+
+print(result) #g**********
+
+
